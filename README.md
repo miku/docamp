3ef37fbf7f689ac89782ee9b4733efd5d05f1785
Martin Czygan <martin.czygan@gmail.com>
2016-11-30 14:00:08 +0100

    update README

--- README.md
+++ README.md
@@ -10,3 +10,30 @@ Adjust solrconfig.xml, in the logs you should see something like:
 ```
 1162 [coreLoadExecutor-3-thread-1] INFO  org.apache.solr.core.SolrResourceLoader  - Adding 'file:/home/tir/code/hsch/solr-minimal/contrib/lib/docamp-1.0-SNAPSHOT.jar' to classloader
 ```
+
+Basic query:
+
+```
+{
+  "responseHeader": {
+    "status": 0,
+    "QTime": 24
+  },
+  "response": {
+    "numFound": 2,
+    "start": 0,
+    "docs": [
+      {
+        "id": "1",
+        "name": "alice",
+        "ExampleTransformer": "From Russia with Love!"
+      },
+      {
+        "id": "2",
+        "name": "bob",
+        "ExampleTransformer": "From Russia with Love!"
+      }
+    ]
+  }
+}
+```
--- src/main/java/de/ubleipzig/solr/transform/HelloTransformerFactory.java
+++ src/main/java/de/ubleipzig/solr/transform/HelloTransformerFactory.java
@@ -3,6 +3,8 @@ package de.ubleipzig.solr.transform;
 import java.io.IOException;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.common.util.StrUtils;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.response.transform.DocTransformer;
 import org.apache.solr.response.transform.TransformerFactory;
@@ -11,30 +13,40 @@ import org.apache.solr.response.transform.TransformerFactory;
  * In solrconfig.xml add:
  *
  * <transformer name="example" class="de.ubleipzig.solr.transform.HelloTransformerFactory">
- *     <str name="location">Russia</str>
+ * <str name="location">Russia</str>
  * </transformer>
- * 
+ *
  * @author Martin Czygan
  */
 public class HelloTransformerFactory extends TransformerFactory {
 
     private String location = "ExampleTransformer";
-    
+
+    @Override
+    public void init(NamedList args) {
+        super.init(args);
+        SolrParams params = SolrParams.toSolrParams(args);
+        String str = params.get("location");
+        if (str != null) {
+            location = str;
+        }
+    }
+
     @Override
     public DocTransformer create(String string, SolrParams sp, SolrQueryRequest sqr) {
         return new ExampleTransformer();
     }
-    
+
     class ExampleTransformer extends DocTransformer {
+
+        @Override
+        public void transform(SolrDocument sd, int i) throws IOException {
+            sd.addField("ExampleTransformer", "From " + location + " with Love!");
+        }
 
         @Override
         public String getName() {
             return "Hello Transformer!";
-        }
-
-        @Override
-        public void transform(SolrDocument sd, int i, float f) throws IOException {
-            sd.addField("ExampleTransformer", "From " + location + " with Love!");
         }
     }
 }
