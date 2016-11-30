5e52b02e250687999e65d7ef1b9fb9238e2f44fe
Martin Czygan <martin.czygan@gmail.com>
2016-11-30 16:32:31 +0100

    a basic query for each document

--- src/main/java/de/ubleipzig/solr/transform/HelloTransformerFactory.java
+++ src/main/java/de/ubleipzig/solr/transform/HelloTransformerFactory.java
@@ -1,13 +1,17 @@
 package de.ubleipzig.solr.transform;
 
 import java.io.IOException;
+import java.util.logging.Logger;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
-import org.apache.solr.common.util.StrUtils;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.response.transform.DocTransformer;
 import org.apache.solr.response.transform.TransformerFactory;
+import org.apache.solr.search.SolrIndexSearcher;
 
 /**
  * In solrconfig.xml add:
@@ -20,6 +24,7 @@ import org.apache.solr.response.transform.TransformerFactory;
  */
 public class HelloTransformerFactory extends TransformerFactory {
 
+    // location
     private String location = "ExampleTransformer";
 
     @Override
@@ -34,14 +39,34 @@ public class HelloTransformerFactory extends TransformerFactory {
 
     @Override
     public DocTransformer create(String string, SolrParams sp, SolrQueryRequest sqr) {
-        return new ExampleTransformer();
+        return new ExampleTransformer(sqr);
     }
 
     class ExampleTransformer extends DocTransformer {
 
+        Logger log = Logger.getLogger(ExampleTransformer.class.getName());
+
+        private SolrQueryRequest sqr;
+
+        ExampleTransformer(SolrQueryRequest sqr) {
+            this.sqr = sqr;
+        }
+
         @Override
         public void transform(SolrDocument sd, int i) throws IOException {
+            SolrIndexSearcher searcher = this.sqr.getSearcher();
+            String[] fields = new String[]{"name"};
+
+            // TODO(miku): create a suitable query
+            // Query query = new MoreLikeThisQuery("alice", fields, searcher.getSchema().getAnalyzer(), "name");
+            Query query = new MatchAllDocsQuery();
+            TopDocs docs = searcher.search(query, 3);
+
+            log.info("Hello from ExampleTransformer: " + query.toString());
+
+            // Query on the fly.
             sd.addField("ExampleTransformer", "From " + location + " with Love!");
+            sd.addField("ExampleTransformerHeader", String.format("%d", docs.totalHits));
         }
 
         @Override
